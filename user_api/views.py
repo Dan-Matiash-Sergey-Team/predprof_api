1e39745e2bddef031f314f378b2353636204d690
Universai <solomatin377@gmail.com>
2021-03-19 18:57:33 +0300

    a

--- user_api/views.py
+++ user_api/views.py
@@ -137,8 +137,8 @@ class RecordView(APIView):
 
     def put(self, request):
         data = request.data.get('record')
-        data['date'] = datetime.strptime(data['date'], '%Y-%m-%d')
         print(data['date'])
+        data['date'] = datetime.strptime(data['date'], '%Y-%m-%d')
         record = get_object_or_404(Record.objects.filter(user=request.user), date__date=data['date'].date())
         serializer = RecordSerializer(instance=record, data=data, partial=True)
         print(serializer.is_valid(raise_exception=True))
