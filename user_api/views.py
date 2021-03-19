6a9f1e45eadcf76110d360d61b1b5fe8e1c5b2ef
Universai <solomatin377@gmail.com>
2021-03-19 17:40:00 +0300

    a

--- user_api/views.py
+++ user_api/views.py
@@ -137,6 +137,7 @@ class RecordView(APIView):
 
     def put(self, request):
         data = request.data.get('record')
+        print(Record.objects.filter(user=request.user, date__date=datetime.strptime(data['date'], "%Y-%m-%d").date()))
         record = get_object_or_404(
             Record.objects.filter(user=request.user, date__date=datetime.strptime(data['date'], "%Y-%m-%d").date()))
         print(data)
