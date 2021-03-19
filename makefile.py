d62df678c3bf063a81c03e77eea8144ad5cfd83e
Your Name <you@example.com>
2021-03-19 19:39:07 +0300

    a

--- makefile.py
+++ makefile.py
@@ -1,5 +1,6 @@
 import os
 
-os.system('python3 manage.py makemigrations')
+os.system('git pull')
+os.system('python3 manage.py makemigrations user_api')
 os.system('python3 manage.py migrate')
 os.system('python3 manage.py runserver 195.133.147.101:228')
