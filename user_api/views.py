2754093ff2aefe2ce508732b0b8138b9b2b966ad
Ваше Имя <you@example.com>
2021-03-19 14:26:18 +0300

    a

--- user_api/views.py
+++ user_api/views.py
@@ -80,7 +80,7 @@ def edit_record(request):
         v = request.GET.get('value', None)
         d = request.GET.get('date', None)
         if v:
-            r = Record.objects.get(date__date=datetime(d).date(), user=request.user)
+            r = Record.objects.get(date__date=datetime.strftime(d, "%Y-%m-%dT%H:%M:%SZ"), user=request.user)
             if r:
                 r.value = v
                 r.save()
